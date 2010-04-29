7cd06aa3a0c10953fb07867508ed3c5b3261ffb1
Zhuplev Anton <zhuplev@gmail.com>
2010-04-29 22:39:32 +0000

    DataConsoleFragment class added

--- js/data/console.js
+++ js/data/console.js
@@ -1,3 +1,15 @@
+const DataConsoleAtom = {
+    submission : 1 << 0,
+    message : 1 << 1,
+    contest : 1 << 2,
+};
+//Как бэ не настаиваю на именно таком задании параметров.
+//Поэтому вся проверка "маски" вынесена в отдельные функции, при желании можно переписать.
+
+
+const DataConsoleDateInf = '9';
+
+
 const DataConsole = $.inherit(
     DataAbstract,
     {
@@ -18,12 +30,8 @@ const DataConsole = $.inherit(
         },
         
         start : function() {
-            var timer = utils.timer(utils.delegate(this, this.sendRequest), 5000);
-        },
-        
-        sendFisrtRequest : function() {
-            jQuery.ajax.post(utils.getMainURL(), this.getRequest(), utils.delegate(this, this.firstResponse));
-            this.json = {};
+            //var timer = utils.timer(utils.delegate(this, this.sendRequest), 5000);
+            
         },
         
         sendRequest : function() {
@@ -40,8 +48,8 @@ const DataConsole = $.inherit(
             for (var key in this.lists) {
                 this.updateData(this.lists[key], dataResponse.console[key]);
             }
-            //$('#message_to_admin').html(XMLHttpRequest.responseText);
-            this.interfaceUpdateEvent();
+            $('#message_to_admin').html(XMLHttpRequest.responseText);
+            //this.interfaceUpdateEvent();
         },
         
         updateIds : function(dataResponse, idType) {
@@ -62,3 +70,60 @@ const DataConsole = $.inherit(
     }
 );
  
+ 
+const DataConsoleFragment = $.inherit(
+    {
+        __constructor : function(since, to, s, m, c) {
+            this.since = since;
+            this.to = to;
+            this.s = s; this.m = m; this.c = c; //submissions, messages, contests
+            this.reset();
+        },
+        
+        reset : function() {
+            this.sc = 0; this.mc = 0; this.cc = 0; //counters
+        },
+        
+        changeMask : function(mask) { //ASK: setMask?
+            this.mask = mask;
+            this.sl = (this.inMask(DataConsoleAtom.submission)) * this.s.length;
+            this.ml = (this.inMask(DataConsoleAtom.message)) * this.m.length;
+            this.cl = (this.inMask(DataConsoleAtom.contest)) * this.c.length;
+            //Длина фрагмента с учётом наложенной маски.
+            //Очень удобно: когда нужно выдать следующий элемент промежутка
+            //т.к. длины "ненужных" массивов обнулены, поэтому просто пишем "слияние" трёх массивов
+            this.length = this.sl + this.ml + this.cl;
+        },
+        
+        setMask : function(mask) { //ASK: loadMask?
+            this.reset();
+            this.changeMask(mask);
+        },
+        
+        inMask : function(value) { //ASK: Fishy name.... How can it be named?
+            return value & this.mask != 0 ? 1 : 0;
+        },
+        
+        getNext : function(mask) {
+            var di = DataConsoleDateInf;
+            var s = (this.sc < this.sl ? this.s[this.sc].time : di);
+            var m = (this.mc < this.ml ? this.m[this.mc].time : di);
+            var c = (this.cc < this.cl ? this.c[this.cc].time : di);
+            var result = String.min(String.min(s, m), c); result = (result == di ? undefined : result);
+            if (!defined(result)) {return result;}
+            
+            if (this.s[this.sc].time == result) {return this.s[this.sc++];}
+            else if (this.c[this.cc].time == result) {return this.c[this.cc++];}
+            else if (this.m[this.mc].time == result) {return this.m[this.mc++];}
+            
+            return result;
+        },
+        
+        getWholeFragment : function() {
+            var result = [];
+            for (var i = 0; i < this.length; i++) {
+                result.push(this.getNext());
+            }
+        },
+    }
+);
